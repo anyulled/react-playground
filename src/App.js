39843f94acbeda51c2b32488d8d818cf89caef4e
alrs <anyul.rivas@gft.com>
2017-04-05 06:39:23 +0200

    replacing flickr api with getty images. including google geolocation api.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends React.Component {
         };
     }
 
-    componentWillMount() {
+    componentDidMount() {
         const WEATHER_API = "bfc079575bff7ec0b8e4a53770e35ec7";
         const IMAGE_SEARCH_API = "afdjswhytex6mdk8dvm8aj2s";
-//http://maps.googleapis.com/maps/api/geocode/json?latlng=41.4925928,2.0763349&sensor=true
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
-                console.info("I've got the position %J", position.coords);
+                //console.info("I've got the position %J", position.coords);
 
                 axios(`http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&lang=es&lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${WEATHER_API}`, {mode: "no-cors"})
                     .then(response => {
@@ -28,41 +28,49 @@ class App extends React.Component {
                             position: position.coords,
                             weather_info: response.data
                         });
+                        axios(`http://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&sensor=true`)
+                            .then(response => {
+                                //console.info(response.data);
+                                this.setState({
+                                    ...this.state,
+                                    city: response.data.results[5].formatted_address
+                                });
+                                axios(`https://api.gettyimages.com/v3/search/images`, {
+                                    headers: {"Api-Key": IMAGE_SEARCH_API},
+                                    params: {
+                                        orientations: "Horizontal",
+                                        phrase: this.state.city,
+                                        page_size: 10
+                                    }
+                                })
+                                    .then(response => {
+                                        //console.info(response.data);
+                                        if (response.data.images && response.data.images.length > 0) {
+                                            this.setState({
+                                                ...this.state,
+                                                photos: response.data.images.map(photo => ({
+                                                    title: photo.title,
+                                                    description: photo.caption,
+                                                    url: photo.display_sizes[0].uri
+                                                }))
+                                            });
+                                        } else {
+                                            this.setState({
+                                                ...this.state,
+                                                error: response.data.message
+                                            });
+                                        }
+                                    })
+                                    .catch(error => {
+                                        console.warn("Getty Images", error);
+                                        this.setState({...this.state, loading: false, error});
+                                    });
+                            }).catch(error => {
+                            console.warn("Google Location error:", error);
+                        });
                     })
                     .catch((error) => {
-                        console.warn(error);
-                        this.setState({...this.state, loading: false, error});
-                    });
-                // const flickr_url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${IMAGE_SEARCH_API}&lat=${position.coords.latitude}&lon=${position.coords.longitude}&per_page=20&format=json&nojsoncallback=1`;
-
-                axios(`https://api.gettyimages.com/v3/search/images`, {
-                    headers: {"Api-Key": IMAGE_SEARCH_API},
-                    params: {
-                        orientations: "Horizontal",
-                        phrase: "barcelona",
-                        page_size: 10
-                    }
-                })
-                // axios(flickr_url)
-                    .then(response => {
-                        console.info(response.data);
-                        // if (response.data.photos && response.data.photos.photo.length > 0) {
-                        //     this.setState({
-                        //         ...this.state,
-                        //         photos: response.data.photos.photo.map(photo => ({
-                        //             title: photo.title,
-                        //             url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_z.jpg`
-                        //         }))
-                        //     });
-                        // } else {
-                        //     this.setState({
-                        //         ...this.state,
-                        //         error: response.data.message
-                        //     });
-                        // }
-                    })
-                    .catch(error => {
-                        console.warn(error);
+                        console.warn("Geolocation error", error);
                         this.setState({...this.state, loading: false, error});
                     });
             }, failure => {
@@ -79,32 +87,35 @@ class App extends React.Component {
             {this.state.loading && <Loader/>}
 
             {!this.state.loading && <Grid fluid>
-                {this.state.weather_info && <PageHeader> {`Hola, ${this.state.weather_info.city.name}!`} </PageHeader>}
+                {this.state.weather_info && <PageHeader> {`Hola, ${this.state.weather_info.city.name}!`}
+                    <small>{this.state.city}</small>
+                </PageHeader>}
                 <Row>
-                    {this.state.error && <Col sm={12}><Alert bsStyle="danger">{this.state.error}</Alert></Col>}
+                    {this.state.error && <Col sm={12}><Alert bsStyle="warning">{this.state.error}</Alert></Col>}
                 </Row>
                 {this.state.weather_info && <Row>
+
+                    {(this.state.photos && this.state.photos.length > 0) &&
+                    <Col sm={3}>
+                        <Carousel>
+                            {this.state.photos.map((photo, index) => (
+                                <Carousel.Item key={index}>
+                                    <img width={361} height={267} alt={photo.title} src={photo.url}/>
+                                    <Carousel.Caption>
+                                        <p>{photo.title}</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>))}
+                        </Carousel>
+                    </Col>
+                    }
                     {this.state.weather_info.list.map((weather, index) => (
-                        <Col sm={index === 0 ? 6 : 3} key={index}>
+                        <Col sm={3} key={index}>
                             <WeatherInfo weather={weather}/>
                         </Col>
                     ))}
                 </Row>}
             </Grid>}
 
-            {(this.state.photos && this.state.photos.length > 0) && <Row>
-                <Col sm={6} smOffset={3}>
-                    <Carousel>
-                        {this.state.photos.map((photo, index) => (
-                            <Carousel.Item key={index}>
-                                <img width={361} height={267} alt="Carousel" src={photo.url}/>
-                                <Carousel.Caption>
-                                    <p>{photo.title}</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>))}
-                    </Carousel>
-                </Col>
-            </Row>}
 
         </div>);
     }
